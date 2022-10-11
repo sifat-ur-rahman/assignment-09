b19cb437d00be3c10bd032473dc53a5cbe344caf
sifat-ur-rahman <md.sifat.ur.rahman2702@gmail.com>
2022-10-11 10:06:20 +0600

    categorie

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,11 +2,12 @@ import React from 'react';
 import img from './quiz-img.png';
 import './Home.css'
 import { useLoaderData } from 'react-router-dom';
+import Categories from '../categories/Categories'
 
 const Home = () => {
     const categories = useLoaderData()
 
-    console.log(categories);
+    console.log(categories.data);
     return (
         <div>
             <div className='header'>
@@ -18,9 +19,12 @@ const Home = () => {
                     <h4>Give this quiz a try and check your knowledge of articles. Let's see if you have been practicing well on your programming. </h4>
                 </div>
             </div>
-            <div>
+            <div className='home-categories'>
                 {
-
+                    categories.data.map(categorie => <Categories
+                        categorie={categorie}
+                        key={categorie.id}
+                    ></Categories>)
                 }
             </div>
         </div>
--- src/Components/categories/categories.js
+++ src/Components/categories/categories.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './categorie.css'
 
-const categories = () => {
+const categories = ({ categorie }) => {
+    const { logo, name, total } = categorie
     return (
-        <div>
-            <h4>categories</h4>
+        <div className='categorie'>
+            <img src={logo} alt="" srcset="" />
+            <h5> Quiz Name: {name}</h5>
+            <p>Total Quiz: {total}</p>
+            <button className='stat-quiz-btn'>Start Quiz</button>
         </div>
     );
 };
