ddbb29bdfd6f4bfdad540e6889793ca7811fafe8
sifat-ur-rahman <md.sifat.ur.rahman2702@gmail.com>
2022-10-11 19:39:21 +0600

    right-ans-2

--- src/Components/Quiz/QuizOption.js
+++ src/Components/Quiz/QuizOption.js
@@ -5,6 +5,8 @@ import './quizoption.css'
 const QuizOption = ({ opt }) => {
     const { correctAnswer, options, question } = opt
     const [ans, setAns] = useState([])
+    const [correctAns, setCorrectAns] = useState([])
+    // console.log(correctAns);
 
     console.log(ans);
     if (correctAnswer === ans) {
@@ -15,7 +17,11 @@ const QuizOption = ({ opt }) => {
 
     return (
         <div className='quizoption'>
-            <h2>Quiz:</h2>
+            <div className='quizAns'>
+                <h2>Quiz Answer: {correctAns}</h2>
+                <button onClick={() => setCorrectAns(correctAnswer)}>A</button>
+            </div>
+
             <h3> {question}</h3>
 
             {
